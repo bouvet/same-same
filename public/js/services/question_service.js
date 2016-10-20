019217bfdbb2ab79e268c933ba87e2f7b0ca1fd2
Simen Sohol <simen.sohol@bouvet.no>
2016-10-20 10:36:27 +0200

    Moved the questions to a folder

--- public/js/services/question_service.js
+++ public/js/services/question_service.js
@@ -15,80 +15,80 @@ angular.module("samesameApp.services.question", [])
                     questions: [
                         {
                             questionid: '1',
-                            imageURLA: "./images/1a.png",
-                            imageURLB: "./images/1b.png"
+                            imageURLA: "./images/questions/1a.png",
+                            imageURLB: "./images/questions/1b.png"
                         }, {
                             questionid: '2',
-                            imageURLA: "./images/2a.png",
-                            imageURLB: "./images/2b.png"
+                            imageURLA: "./images/questions/2a.png",
+                            imageURLB: "./images/questions/2b.png"
                         }, {
                             questionid: '3',
-                            imageURLA: "./images/3a.png",
-                            imageURLB: "./images/3b.png"
+                            imageURLA: "./images/questions/3a.png",
+                            imageURLB: "./images/questions/3b.png"
                         }, {
                             questionid: '4',
-                            imageURLA: "./images/4a.png",
-                            imageURLB: "./images/4b.png"
+                            imageURLA: "./images/questions/4a.png",
+                            imageURLB: "./images/questions/4b.png"
                         }, {
                             questionid: '5',
-                            imageURLA: "./images/5a.png",
-                            imageURLB: "./images/5b.png"
+                            imageURLA: "./images/questions/5a.png",
+                            imageURLB: "./images/questions/5b.png"
                         }, {
                             questionid: '6',
-                            imageURLA: "./images/6a.png",
-                            imageURLB: "./images/6b.png"
+                            imageURLA: "./images/questions/6a.png",
+                            imageURLB: "./images/questions/6b.png"
                         }, {
                             questionid: '7',
-                            imageURLA: "./images/7a.png",
-                            imageURLB: "./images/7b.png"
+                            imageURLA: "./images/questions/7a.png",
+                            imageURLB: "./images/questions/7b.png"
                         }, {
                             questionid: '8',
-                            imageURLA: "./images/8a.png",
-                            imageURLB: "./images/8b.png"
+                            imageURLA: "./images/questions/8a.png",
+                            imageURLB: "./images/questions/8b.png"
                         }, {
                             questionid: '9',
-                            imageURLA: "./images/9a.png",
-                            imageURLB: "./images/9b.png"
+                            imageURLA: "./images/questions/9a.png",
+                            imageURLB: "./images/questions/9b.png"
                         }, {
                             questionid: '10',
-                            imageURLA: "./images/10a.png",
-                            imageURLB: "./images/10b.png"
+                            imageURLA: "./images/questions/10a.png",
+                            imageURLB: "./images/questions/10b.png"
                         }, {
                             questionid: '11',
-                            imageURLA: "./images/11a.png",
-                            imageURLB: "./images/11b.png"
+                            imageURLA: "./images/questions/11a.png",
+                            imageURLB: "./images/questions/11b.png"
                         }, {
                             questionid: '12',
-                            imageURLA: "./images/12a.png",
-                            imageURLB: "./images/12b.png"
+                            imageURLA: "./images/questions/12a.png",
+                            imageURLB: "./images/questions/12b.png"
                         }, {
                             questionid: '13',
-                            imageURLA: "./images/13a.png",
-                            imageURLB: "./images/13b.png"
+                            imageURLA: "./images/questions/13a.png",
+                            imageURLB: "./images/questions/13b.png"
                         }, {
                             questionid: '14',
-                            imageURLA: "./images/14a.png",
-                            imageURLB: "./images/14b.png"
+                            imageURLA: "./images/questions/14a.png",
+                            imageURLB: "./images/questions/14b.png"
                         }, {
                             questionid: '15',
-                            imageURLA: "./images/15a.png",
-                            imageURLB: "./images/15b.png"
+                            imageURLA: "./images/questions/15a.png",
+                            imageURLB: "./images/questions/15b.png"
                         }, {
                             questionid: '16',
-                            imageURLA: "./images/16a.png",
-                            imageURLB: "./images/16b.png"
+                            imageURLA: "./images/questions/16a.png",
+                            imageURLB: "./images/questions/16b.png"
                         }, {
                             questionid: '17',
-                            imageURLA: "./images/17a.png",
-                            imageURLB: "./images/17b.png"
+                            imageURLA: "./images/questions/17a.png",
+                            imageURLB: "./images/questions/17b.png"
                         }, {
                             questionid: '18',
-                            imageURLA: "./images/18a.png",
-                            imageURLB: "./images/18b.png"
+                            imageURLA: "./images/questions/18a.png",
+                            imageURLB: "./images/questions/18b.png"
                         }, {
                             questionid: '19',
-                            imageURLA: "./images/19a.png",
-                            imageURLB: "./images/19b.png"
+                            imageURLA: "./images/questions/19a.png",
+                            imageURLB: "./images/questions/19b.png"
                         }
                     ]
                 };
